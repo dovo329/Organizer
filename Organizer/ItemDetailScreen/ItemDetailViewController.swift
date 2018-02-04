a6d427071158a5b204ecc6d8593d0862ca900895
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 22:20:08 -0700

    Populate views for ItemDetailVC

--- Organizer/ItemDetailScreen/ItemDetailViewController.swift
+++ Organizer/ItemDetailScreen/ItemDetailViewController.swift
@@ -11,6 +11,10 @@ import CoreData
 
 class ItemDetailViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descTextView: UITextView!
+    
     var item: Item?
     
     override func viewDidLoad() {
@@ -18,5 +22,37 @@ class ItemDetailViewController: UIViewController {
         
         let itemName = item?.name ?? ""
         print("item name: \(itemName)")
+        
+        nameTextField.text = item?.name ?? ""
+        descTextView.text = item?.desc ?? ""
+        
+        setDescTextViewBorder()
+    }
+    
+    func setDescTextViewBorder() {
+        descTextView.layer.borderWidth = 1.0
+        descTextView.layer.borderColor = UIColor.lightGray.cgColor
+    }
+}
+
+// MARK: UITextFieldDelegate
+extension ItemDetailViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == nameTextField {
+            if let name = textField.text {
+                self.item?.name = name
+            }
+        }
+    }
+}
+
+// MARK: UITextViewDelegate
+extension ItemDetailViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView == descTextView {
+            if let desc = textView.text {
+                self.item?.desc = desc
+            }
+        }
     }
 }
