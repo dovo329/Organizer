a16511c439ef9424e3643e68c176b7b648bbbfe4
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 20:37:56 -0700

    Add SegueId for constant string literals to project

---
+++ Organizer/Constants/SegueId.swift
@@ -0,0 +1,14 @@
+//
+//  SegueId.swift
+//  Organizer
+//
+//  Created by Douglas Voss on 2/3/18.
+//  Copyright © 2018 VossWareLLC. All rights reserved.
+//
+
+import Foundation
+
+struct SegueId {
+    static let ShowItemList = "ShowItemList"
+    static let ShowItemDetail = "ShowItemDetail"
+}
