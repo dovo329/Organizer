eb5d5fe26eb6f1fd2ef4abf9e90a9c48d9fcd691
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 20:44:31 -0700

    Add category var and debug print statement

--- Organizer/ItemListScreen/ItemListViewController.swift
+++ Organizer/ItemListScreen/ItemListViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 
 class ItemListViewController: UIViewController {
     
+    var category: Category?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let categoryName = category?.name ?? ""
+        print("ItemListViewController category: \(categoryName)")
     }
 }
