1409019efe6188d2be5d58134a548a685190bfbf
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 17:51:48 -0700

    Add ItemListViewController to project

---
+++ Organizer/ItemListScreen/ItemListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemListViewController.swift
+//  Organizer
+//
+//  Created by Douglas Voss on 2/3/18.
+//  Copyright © 2018 VossWareLLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
