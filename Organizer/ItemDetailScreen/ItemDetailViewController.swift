60c161ddecff2b81e9ccfacd93db75081cd6f4b1
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 17:53:39 -0700

    Add ItemDetailViewController to project

---
+++ Organizer/ItemDetailScreen/ItemDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ItemDetailViewController.swift
+//  Organizer
+//
+//  Created by Douglas Voss on 2/3/18.
+//  Copyright © 2018 VossWareLLC. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
