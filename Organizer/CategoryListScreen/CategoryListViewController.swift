ef0d393b7714ba8802849d315b32d9fe31775241
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 18:12:30 -0700

    Add placeholder tableview

--- Organizer/CategoryListScreen/CategoryListViewController.swift
+++ Organizer/CategoryListScreen/CategoryListViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class CategoryListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let categoryCellId = "CategoryCellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: categoryCellId)
     }
 }
 
+// MARK: UITableViewDataSource
+extension CategoryListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        return tableView.dequeueReusableCell(withIdentifier: categoryCellId, for: indexPath)
+    }
+}
+
+// MARK: UITableViewDelegate
+extension CategoryListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+
+        cell.textLabel?.text = "Test \(indexPath.row)"
+    }
+}
