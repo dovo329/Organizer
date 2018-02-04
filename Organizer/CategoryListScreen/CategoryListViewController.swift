64f9ca27793b6a3d8bb5c1b61d1d4379f87da802
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 20:11:10 -0700

    Implement CoreData storage and fetch for Category

--- Organizer/CategoryListScreen/CategoryListViewController.swift
+++ Organizer/CategoryListScreen/CategoryListViewController.swift
@@ -7,24 +7,84 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet weak var addButton: UIBarButtonItem!
     
-
     let categoryCellId = "CategoryCellId"
+    var dataSource = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: categoryCellId)
+        
+        updateDataSourceFromCoreData()
     }
     
     @IBAction func addButtonAction(_ sender: UIBarButtonItem) {
         print("Add action")
+        
+        let alert = UIAlertController(title: NSLocalizedString("Enter Category Name", comment: "Alert title"), message: NSLocalizedString("", comment: "Alert message"), preferredStyle: UIAlertControllerStyle.alert)
+        
+        let saveAction = UIAlertAction(title: NSLocalizedString("Save", comment: "Alert action"), style: UIAlertActionStyle.default) { action in
+            
+            guard let textField = alert.textFields?.first,
+                let categoryName = textField.text else {
+                return
+            }
+            
+            self.addCategoryToCoreData(categoryName: categoryName)
+            self.updateDataSourceFromCoreData()
+            
+            self.tableView.reloadData()
+        }
+        alert.addAction(saveAction)
+        
+        let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: "Alert action"), style: UIAlertActionStyle.cancel)
+        alert.addAction(cancelAction)
+        
+        alert.addTextField()
+        
+        present(alert, animated: true)
+    }
+    
+    func updateDataSourceFromCoreData() {
+        guard let appDelegate =
+            UIApplication.shared.delegate as? AppDelegate else {
+                print("Error, can't access AppDelegate")
+                return
+        }
+        
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest =
+            NSFetchRequest<NSManagedObject>(entityName: "Category")
+        
+        do {
+            let categories = try managedContext.fetch(fetchRequest) as! [Category]
+            dataSource = categories.map({(category: Category) -> (String) in
+                return category.name!
+            })
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func addCategoryToCoreData(categoryName: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let category = NSEntityDescription.insertNewObject(
+            forEntityName: "Category",
+            into: appDelegate.persistentContainer.viewContext) as! Category
+        category.name = categoryName
+        appDelegate.saveContext()
     }
 }
 
@@ -32,7 +92,7 @@ class CategoryListViewController: UIViewController {
 extension CategoryListViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return dataSource.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -46,6 +106,6 @@ extension CategoryListViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
 
-        cell.textLabel?.text = "Test \(indexPath.row)"
+        cell.textLabel?.text = dataSource[indexPath.row]
     }
 }
