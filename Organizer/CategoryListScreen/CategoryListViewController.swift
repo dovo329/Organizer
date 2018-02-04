2c1c50d9a7a617cbf29bec9b3b63e9ecf0cf5aaa
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 17:49:54 -0700

    Add CategoryListViewController to project
    This is renamed from ViewController.  The CategoryListViewController has been embedded in a NavigationController on the storyboard.

---
+++ Organizer/CategoryListScreen/CategoryListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Organizer
+//
+//  Created by Douglas Voss on 2/3/18.
+//  Copyright © 2018 VossWareLLC. All rights reserved.
+//
+
+import UIKit
+
+class CategoryListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
