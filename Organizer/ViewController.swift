3897c09ac96c589c94271231fc90d0a257e59c35
Douglas Voss <douglas.c.voss@gmail.com>
2018-02-03 17:31:13 -0700

    Initial commit

---
+++ Organizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Organizer
+//
+//  Created by Douglas Voss on 2/3/18.
+//  Copyright © 2018 VossWareLLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
